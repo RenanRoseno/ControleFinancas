7ff2e32c64f15884a74fbfc87898b4869317cde6
Renan Roseno <rosenorenan@gmail.com>
2020-05-26 14:55:43 -0300

    Criação da view de tranferência

---
+++ resources/views/admin/balance/transfer_confirm.blade.php
@@ -0,0 +1,37 @@
+@extends('adminlte::page')
+
+@section('title', 'Sistema de Saldo')
+
+
+@section('content_header')
+<h1>Fazer Transferência (APENAS EM R$)</h1>
+
+<ol class="breadcrumb">
+    <li><a href=""> Home</a></li>
+    <li><a href=""> Saldo </a></li>
+</ol>
+@stop
+
+@section('content')
+<div class="box">
+
+    <div class="box-body">
+        <div class="box-header">
+        <h4><strong>Recebedor:</strong> {{$usuario->name}}</h4>    
+        <h3>Valor da Transferência</h3>
+        </div>
+        @include('admin.includes.alerts')
+        <form method="post" action="{{route('transfer.send')}}">
+            {!! csrf_field() !!}  
+
+            <input type="hidden" name="usuario_id" value="{{$usuario->id}}"> 
+            <div class="form-group">
+                <input type="text" name="val" class="form-control" placeholder="Ex: 1000.00" required>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-success">Transferir</button>
+            </div>
+        </form>
+    </div>
+</div>
+@stop
