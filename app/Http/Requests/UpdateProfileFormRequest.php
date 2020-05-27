744519515778b38032b20199ca70100fcc088f6d
Renan Roseno <rosenorenan@gmail.com>
2020-05-27 16:45:42 -0300

    Alterações finais em relação a parsonalização de perfis

--- app/Http/Requests/UpdateProfileFormRequest.php
+++ app/Http/Requests/UpdateProfileFormRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateProfileFormRequest extends FormRequest
+
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -23,8 +24,13 @@ class UpdateProfileFormRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            //
-        ];
+        $id = auth()->user()->id;
+        return[];
+        /*return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users, email, {$id}, id',
+            'password' => 'min:6',
+            'image' => 'image'
+        ];*/
     }
 }
--- resources/views/admin/profile/profile.blade.php
+++ resources/views/admin/profile/profile.blade.php
@@ -9,14 +9,20 @@
 @section('content')
 <div class="box">
     <div class="box-body">
-    @include('admin.includes.alerts')
+        @include('admin.includes.alerts')
         <form action="{{route('profile.update')}}" method="POST" enctype="multipart/form-data">
             {!! csrf_field() !!}
-            @if(auth()->user()->image != null)
             <div class="form-group">
-                <img src="{{url('storage/users/'.auth()->user()->image)}}" class="img-thumbnail" style="max-width:200px;max-height:200px;">
+                @if(auth()->user()->image != null)
+                <img src="{{url('storage/users/'.auth()->user()->image)}}" class="img-thumbnail" style="max-width:200px;max-height:200px;"><br>
+                @endif
+                <label class="form-label">
+                <label class="form-label">
+                    <h4>Mudar imagem de perfil</h4>
+                </label>
+                    <input type="file" class="file" name="image">
+                </label>
             </div>
-            @endif
             <div class="form-group">
                 <label class="form-label">
                     <h4>Nome</h4>
@@ -34,12 +40,6 @@
                     <h4>Senha</h4>
                 </label>
                 <input type="password" class="form-control" name="password">
-            </div>
-            <div class="form-group">
-                <label class="form-label">
-                    <h4>Imagem</h4>
-                </label>
-                <input type="file" class="file" name="image">
             </div>
             <div class="form-group">
                 <button type="submit" class="btn btn-success">Atualizar perfil</button>
